e2c105e5b31d01a0a0f692008d8d2b583962790f
Kevin Nazario <kevin.l.nazario@gmail.com>
2024-02-08 00:55:31 -0800

    Update project files

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # 10_SQL_Alchemy_Project
+
+This project takes a sqlite database containing weather station data and conducts analysis on the database using SQL Alchemy. In this project, I reflected the database to extract/save table references and then was able to create a session to connect to the database using Python/SQL Alchemy. Within the database, there are 2 tables (Measurement and Station), which could be queried for various datapoints. This project focused on the most recent 12 months of data from the database, looking at precipitation and temperature data. Finally, I created a Flask application in order to display various pieces of information in JSON format, with 2 routes dependent on user-input for generating a date-range to query. 
+
+Flask App Notes
+- 
+Precipitation route utilizes average daily precipitation. This was done in order to provide valid info for each day in the last 12 months and take the average of the multiple stations that reported for each day. Averaging will be easier to scale over time (if stations are removed/added) and also provides a value more comparable to a single station (rather than summing). 
+
+Start Route and Start/End Date Route both require user input in YYYY-MM-DD format
+
+Code Source
+- 
+During exploratory precipitation analysis, I used .fromisoformat() in order to convert date into usable datetime format for manipulation. This method was introduced to me during a tutoring session. 
+https://docs.python.org/3/library/datetime.html#datetime.date.fromisoformat
+
+During exploratory station analyis, I originally utilized a .distinct().count() method to calculate the number of stations. Working with a tutor, it was suggested to align query with SQL functionality by doing the functions within the query, so I moved my query to use func.count(distinct()). Both are workable solutions are are provided below, but I did have to update dependencies for SQL Alchemy to include distinct: 
+Original query: 
+session.query(Measurement.station).distinct().count()
+Updated query: 
+session.query(func.count(distinct(Measurement.station))).scalar()
+
+Additonally, the tutor introduced me to the scalar method in order to strip the list/tuple groupings from query results for a single result (displays first element of first result). 
+https://docs.sqlalchemy.org/en/20/orm/queryguide/query.html#sqlalchemy.orm.Query.scalar
+
+
+
--- Surfs_Up/app.py
+++ Surfs_Up/app.py
@@ -5,7 +5,6 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 import numpy as np
-import pandas as pd
 import datetime as dt
 #################################################
 # Database Setup
@@ -37,24 +36,43 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return ( 
-        f"<strong><u>Currently Available Routes:</u></strong><br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<strong>START_DATE</strong>- User enters date in YYYY-MM-DD format<br/>"
-        f"/api/v1.0/<strong>START_DATE/END_DATE</strong> -User enters start & end dates in YYYY-MM-DD format"
+        # print/return strings for all available routes, using <strong> to emphasize user input needed. Added direct url links for routes without user-input
+        f"<strong><u>Currently Available Routes:</u></strong><br/><br/>"
+        
+        f"Precipitation Route- Relative URL: <a href = '/api/v1.0/precipitation'> /api/v1.0/precipitation</a><br/><br/>"
+        
+        f"Stations Route- Relative URL: <a href = '/api/v1.0/stations'>/api/v1.0/stations</a><br/><br/>"
+        
+        f"Temperature Route- Relative URL:<a href = '/api/v1.0/tobs'>/api/v1.0/tobs</a><br/><br/>"
+        
+        f"Start Date Route- Relative URL:/api/v1.0/<strong>START_DATE</strong><br/>\
+        User enters start date in YYYY-MM-DD format. \
+        Valid Date Range: (2016-08-23 - 2017-08-23)<br/>\
+        The Start Date Route displays the minimum temperature, average temperature, and max temperature for the date range of \
+        user-entered start date to the end of the dataset (2017-08-23)<br/><br/>"
+        
+        f"Start Date/End Date Route- Relative URL:/api/v1.0/<strong>START_DATE/END_DATE</strong> <br/>\
+        User enters start & end dates in YYYY-MM-DD format.\
+        Valid Date Range: (2016-08-23 - 2017-08-23)<br/>\
+        The Start Date/End Date Route displays the minimum temperature, average temperature, and max temperature\
+        for the date range of user-entered start date to the user-entered end date"
 )
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    #query most recent date in dataset using max function
     recent_date = session.query(func.max(Measurement.date)).scalar()
+    #create dt.date variable for date 12 months prior to most recent date
     last12mo_date = dt.date.fromisoformat(recent_date) - dt.timedelta(days =365)
-    last12mo_df= session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= last12mo_date).all()
-    dict = {}
-    for date,percip in last12mo_df:
-        dict[date] = percip
+    #query last 12 months of precipitation data, grouping by date to get daily average precipitation from reporting stations
+    last12mo_df= session.query(Measurement.date, func.round(func.avg(Measurement.prcp),2))\
+        .filter(Measurement.date >= last12mo_date).group_by(Measurement.date).all()
 
-    return jsonify(dict)
+    #Use for loop/dictionary comprehenstion in order to create a dictionary with the date as key, and percip as value
+    rain_dict = {date:percip for date,percip in last12mo_df}
+    
+    #return dictionary in json format
+    return jsonify(rain_dict)
     
 @app.route("/api/v1.0/stations")
 def stations():
@@ -77,17 +95,22 @@ def tobs():
     return jsonify(temp_list)
 
 @app.route("/api/v1.0/<start>")
-def start():
+def start_date(start):
+    search_date = dt.date.fromisoformat(start)
+    temps = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs),func.max(Measurement.tobs)).filter(Measurement.date >= search_date).all()
+    temp_stats = list(np.ravel(temps))
 
-
-    return "Test"
+    return jsonify(temp_stats)
 
 @app.route("/api/v1.0/<start>/<end>")
-def start_end():
+def start_and_end(start, end):
+    start_date = dt.date.fromisoformat(start)
+    end_date = dt.date.fromisoformat(end)    
+    temps = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs),func.max(Measurement.tobs)).filter(Measurement.date >= start_date)\
+    .filter(Measurement.date <= end_date).all()
+    temp_stats = list(np.ravel(temps))
 
-
-    return "Test"
-
+    return jsonify(temp_stats)
 
 if __name__ == '__main__':
     app.run(debug=True)
